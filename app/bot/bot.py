2548a115db02374b280cc367e1aaa9ed93eaa33a
Ilya <clplaycom@gmail.com>
2025-02-24 22:55:58 +0500

    fixes

--- app/bot/bot.py
+++ app/bot/bot.py
@@ -1,18 +1,24 @@
 import asyncio
 
+import sys
+print(sys.path)
+
+from ..config import BOT_TOKEN
+
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 
-from app.config import BOT_TOKEN
 
 
-#scheduler = AsyncIOScheduler(timezone='Europe/Moscow')
-#admins = [int(admin_id) for admin_id in config('ADMINS').split(',')]
+# todo tests
+# scheduler = AsyncIOScheduler(timezone='Europe/Moscow')
+# admins = [int(admin_id) for admin_id in config('ADMINS').split(',')]
 
 bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher(storage=MemoryStorage())
+
 
 async def main():
     # scheduler.add_job(send_time_msg, 'interval', seconds=10)
@@ -20,6 +26,7 @@ async def main():
     dp.include_router(start_router)
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
